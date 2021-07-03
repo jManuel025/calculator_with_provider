9b2cc04bd98ecf0089d7f0c7aea1921d7316f77e
jManuel025 <mac.jmanuel@gmail.com>
2021-07-03 18:53:38 -0500

    feat: function to operate numbers

--- lib/src/provider/NumberInputProvider.dart
+++ lib/src/provider/NumberInputProvider.dart
@@ -2,12 +2,12 @@ import 'package:flutter/cupertino.dart';
 
 class NumberInputProvider extends ChangeNotifier {
   String _number = '0';
+  double? _firstOp;
+  double? _secondOp;
   String _operator = '';
 
-  List<double> _numbersToOperate = [];
-
   String get value {
-    return _number;
+    return _removeZeroDecimal(_number);
   }
 
   set value(String character) {
@@ -35,6 +35,7 @@ class NumberInputProvider extends ChangeNotifier {
       case 'RESET':
         {
           _number = '0';
+          _clear();
         }
         break;
       case 'DEL':
@@ -45,29 +46,55 @@ class NumberInputProvider extends ChangeNotifier {
         break;
       case '=':
         {
-          _doOperation();
+          if (_secondOp == null) {
+            _secondOp = double.parse(_number);
+          }
+          if (_operator != '') {
+            _number = _doOperation(_operator);
+          }
+          _clear();
         }
         break;
       default:
         {
-          _operator = character;
-          print(_operator);
-          if (_operator != '') {
-            _numbersToOperate.add(double.parse(_number));
+          if (_firstOp == null) {
+            _firstOp = double.parse(_number);
+            _number = '0';
+          } else {
+            _secondOp = double.parse(_number);
+            _firstOp = double.parse(_doOperation(_operator));
+            _secondOp = null;
+            _number = '0';
           }
-          _operator = '';
-          print(_operator);
+          _operator = character;
         }
     }
   }
 
-  String _doOperation() {
-    // TODO: end fucntion
-    return '';
+  String _doOperation(String opSymbol) {
+    Map<String, dynamic> operation = {
+      '+': _firstOp! + _secondOp!,
+      '-': _firstOp! - _secondOp!,
+      'x': _firstOp! * _secondOp!,
+      '/': (_firstOp! / _secondOp!).toStringAsFixed(2),
+    };
+    return operation[opSymbol].toString();
+  }
+
+  String _removeZeroDecimal(String number) {
+    double decNumber = double.parse(number);
+    return decNumber
+        .toStringAsFixed(decNumber.truncateToDouble() == decNumber ? 0 : 1);
   }
 
   bool _isNumberOrDot(String character) {
     final RegExp isNumberOrDot = RegExp(r'^[0-9]|\.$');
     return isNumberOrDot.hasMatch(character);
+  }
+
+  void _clear() {
+    _firstOp = null;
+    _secondOp = null;
+    _operator = '';
   }
 }
